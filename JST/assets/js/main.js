294c22915bbce1889081e4b18458b8c5305ea03c
guliyevaminesh <146056374+guliyevaminesh@users.noreply.github.com>
2023-10-20 00:03:44 +0400

    Add files via upload

---
+++ JST/assets/js/main.js
@@ -0,0 +1,106 @@
+// let a = 9
+// let b = 8
+
+// if (a>b) {
+//     console.log('a b-den boyukdur');
+// } else if (a=b) {
+//     console.log('a b-ye beraberdir');
+    
+// } else {
+//     console.log('a b-den kicikdir');
+    
+// }
+
+
+let students=[
+
+    {id:1, name: "Ali", surname: "Alisoy", age:20, grade: 95},
+    
+    {id:2, name: "Vusala", surname: "Nabiyeva", age:34, grade: 77},
+    
+    {id:3, name: "Aliya", surname: "Gurbanzade", age:14, grade: 98},
+    
+    {id:4, name: "Sabir", surname: "Nuruyev", age:15, grade: 54},
+    
+    {id:5, name: "Mehman", surname: "Heydarov", age:22, grade: 100},
+    
+    {id:6, name: "Sevana", surname: "Mammadli", age:41, grade: 35},
+    
+    {id:7, name: "Ilham", surname: "Babayev", age:24, grade: 95},
+    
+    {id:8, name: "Namiq", surname: "Gulahmedov", age:20, grade: 95},
+    
+    {id:9, name: "Aygun", surname: "Kazimova", age:24, grade: 100}
+    
+    ]
+
+// 2.
+//     let cutTelebeler = [];
+
+//     for (let i = 0; i < students.length; i++) {
+//     if (students[i].id % 2 === 0) {
+//         cutTelebeler.push(students[i].name);
+//     }
+// }
+
+// 3.
+//     console.log(cutTelebeler);
+
+// let tekTelebeler = [];
+
+// for (let i = 0; i < students.length; i++) {
+// if (students[i].id % 2 === 1) {
+//     tekTelebeler.push(students[i].name);
+// }
+// }
+
+// console.log( tekTelebeler);
+
+// 4.
+// let telebeYasi = [];
+// for (let i = 0; i < students.length; i++){
+//     if (students[i].age>20) {
+//         telebeYasi.push(students[i].name);
+//     }
+//     }
+    
+//     console.log( telebeYasi);
+
+// 5.
+// let umumTelebe = [];
+// for (let i = 0; i < students.length; i++){
+//     if (students[i].id % 2 === 0 && students[i].grade>90) {
+//        umumTelebe.push(students[i].name);
+//     }
+//     }
+    
+//     console.log( umumTelebe);
+
+// 6.
+// let telebegrade = [];
+// for (let i = 0; i < students.length; i++){
+//     if (students[i].grade==95 || students[i].grade==100) {
+//        telebegrade.push(students[i].name);
+//     }
+//     }
+    
+//     console.log( telebegrade);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
